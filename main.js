d9d21826ca4a3ffce0b95e47ab02f9cfc86d7841
1amanton <iamantongit@gmail.com>
2022-02-22 16:13:58 +0300

    lesson 2

--- main.js
+++ main.js
@@ -1,47 +1,133 @@
-const products = [
-    {
-        id: 1,
-        title: "MacBook",
-        price: 100,
-        image: "./img/macbook.png"
-    },
-
-    {
-        id: 2,
-        title: "Watch",
-        price: 110,
-        image: "./img/watch.png"
-    },
-
-    {
-        id: 3,
-        title: "iPhone",
-        price: 120,
-        image: "./img/iphone.png"
-    },
-
-    {
-        id: 4,
-        title: "TV",
-        price: 130,
-        image: "./img/tv.png"
-    },
-];
-
 const productsDiv = document.querySelector(".productlist");
 
-const renderProduct = product => {
-    return `<div class="product">
-                <img class="product-img" src="${product.image}">
-                <h3 class="product-title">Apple ${product.title}</h3>
-                <p class="product-price">$ ${product.price}</p>
-                <button class="product-buy">Buy!</button>
-            </div>`
+class ProductList {
+    constructor(container=productsDiv) {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+        this.getProductsSum();
+    }
+
+    /**
+     * fetching goods array
+     */
+    _fetchProducts() {
+        this.goods = [
+
+            {
+                id: 1,
+                title: "MacBook",
+                price: 100,
+                image: "./img/macbook.png"
+            },
+        
+            {
+                id: 2,
+                title: "Watch",
+                price: 110,
+                image: "./img/watch.png"
+            },
+        
+            {
+                id: 3,
+                title: "iPhone",
+                price: 120,
+                image: "./img/iphone.png"
+            },
+        
+            {
+                id: 4,
+                title: "TV",
+                price: 130,
+                image: "./img/tv.png"
+            },
+
+        ];
+    };
+
+
+    /**
+     * fint sum of all products
+     */
+    getProductsSum() {
+        let sum = 0;
+        this.goods.forEach(product => {
+            sum += product.price;
+        });
+        console.log(`Сумма всех товаров = ${sum} $`);
+    };
+
+    /**
+     * render goods on page
+     */
+    render() {
+        this.goods.forEach(product => {
+            const item = new ProductItem(product);
+            this.container.insertAdjacentHTML("beforeend", item.render());
+        });
+    };
 };
 
-const renderPage = productlist => {
-    productsDiv.innerHTML = productlist.map(product => 
-        renderProduct(product)).join("");
+
+class ProductItem {
+    constructor(product) {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.image = product.image;
+    };
+
+    /**
+     * render product html
+     */
+    render() {
+        return `<div class="product">
+                    <img class="product-img" src="${this.image}">
+                    <h3 class="product-title">Apple ${this.title}</h3>
+                    <p class="product-price">$ ${this.price}</p>
+                    <button class="product-buy">Buy!</button>
+                </div>`
+    };
 };
 
-renderPage(products);
+let list = new ProductList();
+
+class Cart {
+
+    addProduct() {
+
+    };
+
+    getProductsSum() {
+
+    };
+
+    applyPromocode() {
+
+    };
+
+    makeOrder() {
+
+    };
+};
+
+class CartItem {
+
+    removeProduct() {
+
+    };
+    
+    incrementItem() {
+
+    };
+
+    decrementItem() {
+
+    };
+
+};
+
+
+
+
