703cc501b3d39aef996e6e056af9fc5c10a05a7f
1amanton <iamantongit@gmail.com>
2022-03-15 15:08:36 +0300

    lesson 5 / add Vue cart elements

--- Vue/main.js
+++ Vue/main.js
@@ -7,9 +7,12 @@ const ProductList = new Vue({
 
     data: {
         catalogUrl: "/catalogData.json",
+        cartUrl: "/getBasket.json",
 
         products: [],
         filteredProducts: [],
+        cartProducts: [],
+        
 
         userSearch: "",
 
@@ -28,31 +31,71 @@ const ProductList = new Vue({
             })
         },
 
+        addProduct(item) {
+            this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                if(data.result === 1) {
+
+                    let find = this.cartProducts.find(product => product.id_product == item.id_product);
+                    if(find) {
+                        find.quantity++;
+                    } else {
+                        console.log("ELSE");
+                        //create new object with item parametrs and quantity=0
+                        const prod = Object.assign({quantity: 1}, item);
+                        console.log(prod);
+                        this.cartProducts.push(prod);
+                    };
+
+                } else {
+                    alert("Error addProduct");
+                };
+            });
+        },
+
+        removeProduct(item) {
+            this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                console.log("removing");
+                if(data.result === 1) {
+                    if(item.quantity > 1) {
+                        item.quantity--;
+                    } else {
+                        //if quantity < 1 - remove product from array by splice method
+                        this.cartProducts.splice(this.cartProducts.indexOf(item), 1)
+                    };
+
+                } else {
+                    alert("Error removeProduct");
+                };
+            });
+        },
+
         searchFilter() {
-            
-            console.log("search submited");
-            const regExp = new RegExp(this.userSearch, "i")
-
-            this.filteredProducts = this.products.filter(product => regExp.test(product.product_name))
-            console.log(this.filteredProducts);
-
+            const regExp = new RegExp(this.userSearch, "i");
+            this.filteredProducts = this.products.filter(product => regExp.test(product.product_name));
         },
 
         countCart() {
-            return this.products.length;
+            return this.cartProducts.length;
         },
     },
 
     mounted () {
         this.getJson(`${API}/${this.catalogUrl}`)
-            .then(data => {
-                for (let product of data) {
-                    this.products.push(product);
-                    this.filteredProducts.push(product);
-                    // console.log(this.products);
-                    // console.log(this.filteredProducts);
-                };
-            });
+        .then(data => {
+            for (let product of data) {
+                this.products.push(product);
+                this.filteredProducts.push(product);
+            };
+        });
+
+        this.getJson(`${API}/${this.cartUrl}`)
+        .then(data => {
+            for (let product of data.contents) {
+                this.cartProducts.push(product);
+            };
+        });
 
     },
 });
